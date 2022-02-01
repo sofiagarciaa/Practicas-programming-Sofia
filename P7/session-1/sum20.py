624ef917bafe33aa53a00fbf4ef6c67cb2a4a683
sofiagarciaa <s.garciaal.2021@alumnos.urjc.es>
2022-02-01 16:36:28 +0100

    creation of calculate_weight creation of whatever

---
+++ P7/session-1/sum20.py
@@ -0,0 +1,12 @@
+# Session 1. Exercise 3
+# Calculate the sum of the 20 first integer numbers
+# 1 + 2 + 3 + ... + 20
+# Example working!
+
+# -- Store the result
+res = 0
+
+for i in range(1, 21):
+    res += i
+
+print("Total sum: ", res)
